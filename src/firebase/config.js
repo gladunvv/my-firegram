4651294dd016a9eb090624925f54348f142356fb
vladislav <bincha.1997@gmail.com>
2020-08-05 16:41:53 +1000

    feat: added upload form and usestorage hook

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBcD-XLTqvlqC6uqMpVd_wxXIM0iq2N0DQ',
+  authDomain: 'ninja-firegram-5046b.firebaseapp.com',
+  databaseURL: 'https://ninja-firegram-5046b.firebaseio.com',
+  projectId: 'ninja-firegram-5046b',
+  storageBucket: 'ninja-firegram-5046b.appspot.com',
+  messagingSenderId: '353746793288',
+  appId: '1:353746793288:web:743dc687a2a4b7fdab397d',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
